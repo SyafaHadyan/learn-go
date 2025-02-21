4e05d07783ec31192739cfbaaf7d235d63ccc4e1
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2025-02-21 18:16:42 +0700

    Fetch url prefix

--- book/ch01/fetch/fetch.go
+++ book/ch01/fetch/fetch.go
@@ -5,10 +5,14 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !(strings.HasPrefix(url, "http")) {
+			url = "https://" + url
+		}
 		respond, error := http.Get(url)
 		if error != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", error)
@@ -20,7 +24,7 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, error)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", body)
+		fmt.Printf("%s\n\nStatus: %s\n", body, respond.Status)
 
 	}
 }
